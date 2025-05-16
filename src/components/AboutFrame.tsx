dcab2a83c934a9e661aac9a70acf714ca9426634
Raja Majumdar <raja.majumdar.01@gmail.com>
2025-05-16 23:13:10 +0530

    fix: theme toggle in Frame, about and Grids (#7)
    * feat: Frame and about
    
    * fix: theme toggle in Frame, about and grids
    
    * fix: number format

--- src/components/AboutFrame.tsx
+++ src/components/AboutFrame.tsx
@@ -4,7 +4,7 @@ import GridBackgroud from './ui/GridBackgroud';
 
 const AboutFrame = () => {
   return (
-    <section className='relative md:min-h-screen overflow-hidden z-10'>
+    <section className='relative md:min-h-screen overflow-hidden z-10 text-[--black] dark:text-[--white]'>
       <div className='md:px-32 px-4 py-16 max-w-6xl'>
         <span className='md:text-[27px] text-[21px]'>
           {CONTENT.cfsText}
@@ -13,7 +13,7 @@ const AboutFrame = () => {
         <div className="flex md:gap-16 gap-8 mt-8">
           {CONTENT.cfsStats.map(item =>
             <div className='flex flex-col' key={item.name}>
-              <span className='md:text-4xl text-[27px] w-1/3'>{item.value}+</span>
+              <span className='md:text-4xl text-[27px] w-1/3'>{item.value.toLocaleString()}+</span>
               <span className='text-base'>{item.name}</span>
             </div>
           )}
--- src/components/ui/GridBackgroud.tsx
+++ src/components/ui/GridBackgroud.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { cn } from "@/utils/utils";
+import { useTheme } from "@/context/ThemeContext";
 
 interface GridBackgroundProps {
   className?: string;
@@ -9,6 +10,7 @@ const GridBackgroud: React.FC<GridBackgroundProps> = ({ className = '' }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [rows, setRows] = useState(0);
   const [cols, setCols] = useState(0);
+  const { theme } = useTheme();
 
   useEffect(() => {
     const updateGridSize = () => {
@@ -25,16 +27,19 @@ const GridBackgroud: React.FC<GridBackgroundProps> = ({ className = '' }) => {
     return () => window.removeEventListener('resize', updateGridSize);
   }, []);
 
-  const gridItems = Array.from({ length: rows * cols }, (_, i) => (
-    <div
+  const gridItems = Array.from({ length: rows * cols }, (_, i) => {
+    const bgColor = (Math.random() < 0.1)
+      ? theme === 'dark'
+        ? '#181818' // Dark fill
+        : '#f3f4f6' // Light fill
+      : 'transparent';
+
+    return <div
       key={i}
-      className="w-10 h-10"
-      style={{
-        backgroundColor: Math.random() < 0.2 ? '#f3f4f6' : 'transparent',
-        border: '0.54px solid #ebebeb',
-      }}
+      className={`w-10 h-10 border-[0.54px] border-solid border-[#ebebeb] dark:border-[#181818]`}
+      style={{ backgroundColor: bgColor }}
     />
-  ));
+  });
 
   return (
     <div
